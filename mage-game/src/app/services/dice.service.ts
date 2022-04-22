53adda525d6e058def24226792d18d1cbcaaadec
Kyle Young <dev.kyoung@gmail.com>
2022-04-21 23:14:17 -0700

    clean up + planning

--- mage-game/src/app/services/dice.service.ts
+++ mage-game/src/app/services/dice.service.ts
@@ -1,9 +1,22 @@
+/*
+* Dice Service
+* TODO: 
+* -> convert to injectable class
+* -> track how many times what has been rolled
+* -> create die object
+* -> use conventional dnd dice + others
 
-export const roll = (sides: number) => 1 + Math.floor(Math.random() * sides);
 
-export const rollplus = (sides: number, mod: number) => {
-    return roll(sides) + mod;
-}
+*/
 
-export const two = (base: number) => 2 * base;
+import { coin } from "../enums/coin";
+
+export const roll = (sides: number): number => 1 + Math.floor(Math.random() * sides);
+
+export const rollplus = (sides: number, mod: number): number => roll(sides) + mod;
+
+export const two = (base: number): number => 2 * base;
+
+export const coinFlip = (): coin => roll(2) === 1 ? coin.Heads : coin.Tails;
+
 
